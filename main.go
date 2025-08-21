2b622e0676f8e9a44c2535c0ad90e1c17bb07f4b
Teo Podobnik <teo.podobnik1234@gmail.com>
2025-08-21 09:17:25 +0000

    add detailed code comments

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+// The following line is explained in the tutorial
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpf hello hello.c
 
 import (
@@ -14,19 +15,23 @@ import (
 )
 
 func main() {
-	// Remove resource limits for kernels <5.11.
+	// By default, Linux sets a RLIMIT_MEMLOCK (memory lock limit) that restricts how much memory a process can lock into RAM. 
+	// eBPF maps and programs often use pinned memory that counts against this limit. 
+	// If you don’t raise or remove the limit, loading larger eBPF programs or maps will fail with errors like “operation not permitted” or “memory locked limit exceeded.”
 	if err := rlimit.RemoveMemlock(); err != nil {
 		log.Fatal("Removing memlock:", err)
 	}
 
-	// Load the compiled eBPF ELF and load it into the kernel.
+	// Load the compiled eBPF ELF into the kernel.
+	// These helpers are defined in the generated `hello_bpf.go` file that embeeds the eBPF ELF object file (`hello_bpf.o`)
 	var objs helloObjects
 	if err := loadHelloObjects(&objs, nil); err != nil {
 		log.Fatal("Loading eBPF objects:", err)
 	}
 	defer objs.Close()
 
-	// Attach eBPF Tracepoint
+	// Attach the loaded eBPF program to the eBPF Tracepoint Hook point
+	// The `objs.HandleExecveTp` variable names, follow to function name in our eBPF kernel program
 	tp, err := link.Tracepoint("syscalls", "sys_enter_execve", objs.HandleExecveTp, nil)
 	if err != nil {
 		log.Fatalf("Attaching Tracepoint: %s", err)
@@ -35,7 +40,7 @@ func main() {
 	
 	log.Println("eBPF program attached to tracepoint. Press Ctrl+C to exit.")
 
-	// Wait for SIGINT/SIGTERM
+	// Wait for SIGINT/SIGTERM (Ctrl+C) before exiting
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
